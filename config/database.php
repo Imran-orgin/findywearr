c9bd1a4a5ade07fb0f6d15dd2dcbae07c280959a
Imran-orgin <imranfahmi2002@gmail.com>
2026-06-01 22:06:59 +0530

    Day 1: Project setup - folder structure and database

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'findywears');
+define('DB_NAME', 'findywearce');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
